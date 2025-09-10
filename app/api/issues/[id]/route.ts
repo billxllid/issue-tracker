76b9ca97fa9f0a99536803b00ba2e9835689da5d
billxllid <zangjiaao@yahoo.com>
2025-09-10 14:47:27 +0800

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,29 +1,47 @@
-import { IssueSchema } from "@/app/validationSchemas";
+import { AssignIssueSchema } from "@/app/validationSchemas";
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  // Authenticate the user
   const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  // if (!session) {
+  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // }
 
-  const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
-  if (!validation.success) 
-    return NextResponse.json({ error: validation.error.issues }, { status: 400 });
-
+  // Find the issue
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt((await params).id) },
   });
   if (!issue) 
     return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+
+  // Validate the request body
+  let body;
+  try {
+    body = await request.json();
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid JSON format" }, { status: 400 });
+  }
+  const validation = AssignIssueSchema.safeParse(body);
+  if (!validation.success) 
+    return NextResponse.json({ error: validation.error.issues }, { status: 400 });
   
+  // if the assignedToId is provided, we need to check if the user exists
+  const { assignedToId, title, description } = validation.data;
+  if (assignedToId) {
+    const assignedTo = await prisma.user.findUnique({
+      where: { id: assignedToId },
+    });
+    if (!assignedTo)
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: validation.data.title, description: validation.data.description },
+    data: { title: title, description: description, assignedToId: assignedToId },
   });
   return NextResponse.json(updatedIssue);
 }
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,12 @@ import { z } from "zod";
 
 export const IssueSchema = z.object({
   title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, 'Description is required'),
+  description: z.string().min(1, 'Description is required').max(65535),
+});
+
+
+export const AssignIssueSchema = z.object({
+  title: z.string().min(1, 'Title is required').max(255).optional(),
+  description: z.string().min(1, 'Description is required').max(65535).optional(),
+  assignedToId: z.string().min(1, 'Assigned to is required').max(255).optional(),
 });
